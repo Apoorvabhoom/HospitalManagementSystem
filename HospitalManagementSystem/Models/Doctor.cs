c8191ab9e5a3866af925fc03ca2d6ed42e9d7968
Apoorva <apoorvapally@gmail.com>
2026-03-24 15:49:50 -0400

    tested Doctor

--- HospitalManagementSystem/Models/Doctor.cs
+++ HospitalManagementSystem/Models/Doctor.cs
@@ -13,6 +13,8 @@ namespace HospitalManagementSystem.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        
+
         public ICollection<Patient> Patients { get; set; }
     }
 }
